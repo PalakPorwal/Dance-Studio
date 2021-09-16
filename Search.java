309ca1fa4f1b1da979523544f4339bd480cf44c0
Palak Porwal <55189276+PalakPorwal@users.noreply.github.com>
2021-09-16 20:35:38 +0530

    Add files via upload

---
+++ Search.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author LENOVOO
+ */
+@WebServlet(urlPatterns = {"/Search"})
+public class Search extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String DType=request.getParameter("dtype");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection Con=DriverManager.getConnection("jdbc:mysql://localhost:3306/dancestudio","root","root");
+            Statement st=Con.createStatement();
+            ResultSet rs=st.executeQuery("select * from staff where dtype='"+DType+"'");
+             
+            out.println("<body style=background-color:lightpink;>");
+            out.println("<table>");
+            out.println("<table border=2>");
+            while(rs.next())
+            {
+                out.println("<tr>");
+                
+                out.println("<td>"+rs.getString(1)+"<td>");
+                
+                out.println("<td>"+rs.getString(2)+"<td>");
+               
+                out.println("<td>"+rs.getString(3)+"<td>");
+               
+                out.println("<td>"+rs.getString(4)+"<td>");
+                
+                out.println("<td>"+rs.getString(5)+"<td>");
+                
+            }
+            
+              out.println("</table>");      
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
